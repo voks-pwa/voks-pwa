8eb88d4fb52b12b144e43349d94286ed5582bec5
voks-pwa <voksmedsos@gmail.com>
2026-06-11 18:59:03 +0700

    fix: use https stream url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const AZURACAST_BASE_URL = 'https://a7.alhastream.com/api'
 export const STATION_ID = 42
 export const NOW_PLAYING_POLL_INTERVAL_MS = 15_000
+export const VOKS_STREAM_URL = 'https://a7.alhastream.com:4000/radio'
--- src/services/azuracast/now-playing.ts
+++ src/services/azuracast/now-playing.ts
@@ -1,4 +1,8 @@
-import { AZURACAST_BASE_URL, STATION_ID } from '@/lib/constants'
+import {
+  AZURACAST_BASE_URL,
+  STATION_ID,
+  VOKS_STREAM_URL,
+} from '@/lib/constants'
 import type { AzuraCastNowPlayingResponse } from '@/types/azuracast'
 
 export class AzuraCastApiError extends Error {
@@ -8,6 +12,40 @@ export class AzuraCastApiError extends Error {
     super(message)
     this.name = 'AzuraCastApiError'
     this.status = status
+  }
+}
+
+function normalizeStreamUrl(url: string) {
+  try {
+    const streamUrl = new URL(url)
+
+    if (
+      streamUrl.hostname === 'a7.alhastream.com' &&
+      streamUrl.port === '4000' &&
+      streamUrl.pathname === '/radio'
+    ) {
+      return VOKS_STREAM_URL
+    }
+  } catch {
+    return url
+  }
+
+  return url
+}
+
+function normalizeNowPlayingResponse(
+  data: AzuraCastNowPlayingResponse,
+): AzuraCastNowPlayingResponse {
+  return {
+    ...data,
+    station: {
+      ...data.station,
+      listen_url: normalizeStreamUrl(data.station.listen_url),
+      mounts: data.station.mounts.map((mount) => ({
+        ...mount,
+        url: normalizeStreamUrl(mount.url),
+      })),
+    },
   }
 }
 
@@ -25,5 +63,7 @@ export async function fetchNowPlaying(
     )
   }
 
-  return response.json() as Promise<AzuraCastNowPlayingResponse>
+  const data = (await response.json()) as AzuraCastNowPlayingResponse
+
+  return normalizeNowPlayingResponse(data)
 }
