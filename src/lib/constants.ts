0bef19f4ade9dfd677e1a501b0805b98b90748c0
voks-pwa <voksmedsos@gmail.com>
2026-06-11 18:21:19 +0700

    fix: use https for azuracast endpoints

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const AZURACAST_BASE_URL = 'http://a7.alhastream.com/api'
+export const AZURACAST_BASE_URL = 'https://a7.alhastream.com/api'
 export const STATION_ID = 42
 export const NOW_PLAYING_POLL_INTERVAL_MS = 15_000
